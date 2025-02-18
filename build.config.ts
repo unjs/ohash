fe3db662f38ae8f7c768ed6b3eadc3db4c09b74b
Pooya Parsa <pooya@pi0.io>
2025-02-19 00:56:52 +0100

    build: selectively minify js hash impl

---
+++ build.config.ts
@@ -0,0 +1,26 @@
+import { defineBuildConfig } from "unbuild";
+import { transform } from "esbuild";
+import { rm } from "node:fs/promises";
+
+export default defineBuildConfig({
+  hooks: {
+    "rollup:options"(ctx, opts) {
+      opts.plugins.push({
+        name: "selective-minify",
+        async transform(code, id) {
+          if (id.includes("crypto/js")) {
+            const res = await transform(code, {
+              minify: true,
+            });
+            return res.code;
+          }
+        },
+      });
+    },
+    async "build:done"() {
+      await rm("dist/index.d.ts");
+      await rm("dist/crypto/js/index.d.ts");
+      await rm("dist/crypto/node/index.d.ts");
+    },
+  },
+});
--- test/crypto.test.ts
+++ test/crypto.test.ts
@@ -3,9 +3,12 @@ import { describe, expect, it } from "vitest";
 import * as cryptoJS from "../src/crypto/js";
 import * as cryptoNode from "../src/crypto/node";
 
+// import * as cryptoDistJS from "../dist/crypto/js/index.mjs";
+
 const impls = {
   js: cryptoJS,
   node: cryptoNode,
+  // distJs: cryptoDistJS,
 };
 
 describe("crypto", () => {
