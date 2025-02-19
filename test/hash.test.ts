9de88d389ad522fdfe41f6b6b077918a5cd6a7a8
Pooya Parsa <pooya@pi0.io>
2025-02-20 00:43:20 +0100

    chore: lint

--- test/hash.test.ts
+++ test/hash.test.ts
@@ -3,7 +3,7 @@ import { hash } from "../src";
 
 describe("hash", () => {
   it("hash", () => {
-    const obj = { foo: "bar" }
+    const obj = { foo: "bar" };
     expect(hash(obj)).toMatchInlineSnapshot(
       `"g82Nh7Lh3CURFX9zCBhc5xgU0K7L0V1qkoHyRsKNqA4"`,
     );
