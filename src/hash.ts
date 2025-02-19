80169b51878a4b97a7007322d4e619ab0158d33e
Pooya Parsa <pooya@pi0.io>
2025-02-19 11:22:44 +0100

    update docs

--- src/hash.ts
+++ src/hash.ts
@@ -4,6 +4,8 @@ import { digest } from "ohash/crypto";
 /**
  * Hashes any JS value into a string.
  *
+ * The input is first serialized into a string like `object:1:string:3:foo:string:3:bar,`. It is then hashed and truncated to a length of `10`.
+ *
  * @param {object} object value to hash
  * @param {SerializeOptions} options hashing options. See {@link SerializeOptions}.
  * @return {string} hash value
