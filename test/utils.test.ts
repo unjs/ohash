8079b69f8b65abf6e7e71b3abe311000b88aa7b6
Pooya Parsa <pooya@pi0.io>
2025-02-19 11:47:25 +0100

    chore: fix tests

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { isEqual, diff } from "../src";
+import { isEqual, diff } from "../src/utils";
 
 describe("isEqual", () => {
   const cases = [
