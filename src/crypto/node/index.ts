d9273a38b290d11c48d29b191209c810c710e7fb
Pooya Parsa <pooya@pi0.io>
2025-02-20 18:39:48 +0100

    fix: use `process.getBuiltinModule` to import fast hash (#118)
    * fix: use `process.getBuiltinModule` to import fast hash
    
    * conditional chaining

--- src/crypto/node/index.ts
+++ src/crypto/node/index.ts
@@ -1,4 +1,9 @@
-import { createHash, hash } from "node:crypto";
+import { createHash } from "node:crypto";
+
+// Available in Node.js v21.7.0+, v20.12.0+
+// https://nodejs.org/api/crypto.html#cryptohashalgorithm-data-outputencoding
+const fastHash = /*@__PURE__*/ (() =>
+  globalThis.process?.getBuiltinModule?.("crypto")?.hash)();
 
 /**
  * Hashes a string using the SHA-256 algorithm and encodes it in Base64URL format.
@@ -8,10 +13,8 @@ import { createHash, hash } from "node:crypto";
  * @returns {string} The hash of the data.
  */
 export function digest(data: string): string {
-  if (hash) {
-    // Available in Node.js v21.7.0+, v20.12.0+
-    // https://nodejs.org/api/crypto.html#cryptohashalgorithm-data-outputencoding
-    return hash("sha256", data, "base64url");
+  if (fastHash) {
+    return fastHash("sha256", data, "base64url");
   }
 
   const h = createHash("sha256").update(data);
