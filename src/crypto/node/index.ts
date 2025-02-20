fbca1caed5656a1f53d9d2ab5df1ea73feb354e1
Pooya Parsa <pooya@pi0.io>
2025-02-20 17:49:53 +0100

    perf: use node crypto `hash` when available (#116)

--- src/crypto/node/index.ts
+++ src/crypto/node/index.ts
@@ -1,12 +1,17 @@
-import { createHash } from "node:crypto";
+import { createHash, hash } from "node:crypto";
 
 /**
  * Hashes a string using the SHA-256 algorithm and encodes it in Base64URL format.
  *
- * @param {string} message - The message to hash.
+ * @param {string} data - data message to hash.
  *
- * @returns {string} The hash of the message.
+ * @returns {string} The hash of the data.
  */
-export function digest(date: string): string {
-  return createHash("sha256").update(date).digest("base64url");
+export function digest(data: string): string {
+  if (hash) {
+    // Available in Node.js v21.7.0+, v20.12.0+
+    // https://nodejs.org/api/crypto.html#cryptohashalgorithm-data-outputencoding
+    return hash("sha256", data, "base64url");
+  }
+  return createHash("sha256").update(data).digest("base64url");
 }
