92b67340186e00fc273bcb75f0d13e9998e336e5
Pooya Parsa <pooya@pi0.io>
2025-02-20 17:55:09 +0100

    fix: workaround for stackblitz

--- src/crypto/node/index.ts
+++ src/crypto/node/index.ts
@@ -13,5 +13,7 @@ export function digest(data: string): string {
     // https://nodejs.org/api/crypto.html#cryptohashalgorithm-data-outputencoding
     return hash("sha256", data, "base64url");
   }
-  return createHash("sha256").update(data).digest("base64url");
+  // Use digest().toString("base64url") as workaround for stackblitz
+  // https://github.com/unjs/ohash/issues/115
+  return createHash("sha256").update(data).digest().toString("base64url");
 }
