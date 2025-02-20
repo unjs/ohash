e12dd1894f74bb7eb508e87b02bdedf8c664da34
Pooya Parsa <pooya@pi0.io>
2025-02-20 11:43:17 +0100

    chore: lint

--- test/benchmarks.bench.ts
+++ test/benchmarks.bench.ts
@@ -7,16 +7,18 @@ import { serialize } from "../src";
 describe("benchmarks", () => {
   describe.only("digest", async () => {
     const input = "hello world";
-    const output = "uU0nuZNNPgilLlLX2n2r-sSE7-N6U4DukIj3rOLvzek"
+    const output = "uU0nuZNNPgilLlLX2n2r-sSE7-N6U4DukIj3rOLvzek";
     const implementations = {
-      'js': digestJS,
-      'node:crypto': digestNode,
-      'subtleCrypto': subtleCrypto,
-      'subtleCryptoWithBuffer': subtleCryptoWithNodeBuffer,
-    }
+      js: digestJS,
+      "node:crypto": digestNode,
+      subtleCrypto: subtleCrypto,
+      subtleCryptoWithBuffer: subtleCryptoWithNodeBuffer,
+    };
     for (const [name, digest] of Object.entries(implementations)) {
-      if (await digest(input) !== output) {
-        throw new Error(`digest implementation "${name}" is incorrect: ${await digest(input)}`);
+      if ((await digest(input)) !== output) {
+        throw new Error(
+          `digest implementation "${name}" is incorrect: ${await digest(input)}`,
+        );
       }
       // @ts-ignore (Promise<string>)
       bench(name, () => {
@@ -36,10 +38,15 @@ const encoder = new TextEncoder();
 
 function subtleCrypto(input: string) {
   return crypto.subtle.digest("SHA-256", encoder.encode(input)).then((hash) => {
-    return btoa(String.fromCharCode(...new Uint8Array(hash))).replace(/=/g, "").replace(/\+/g, "-").replace(/\//g, "_");
+    return btoa(String.fromCharCode(...new Uint8Array(hash)))
+      .replace(/=/g, "")
+      .replace(/\+/g, "-")
+      .replace(/\//g, "_");
   });
 }
 
 function subtleCryptoWithNodeBuffer(input: string) {
-  return crypto.subtle.digest("SHA-256", encoder.encode(input)).then((hash) => Buffer.from(hash).toString("base64url"));
+  return crypto.subtle
+    .digest("SHA-256", encoder.encode(input))
+    .then((hash) => Buffer.from(hash).toString("base64url"));
 }
