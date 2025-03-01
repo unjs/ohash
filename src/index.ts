14497affb284aa470127ef03ab4b3f9c2b14a801
Pooya Parsa <pooya@pi0.io>
2025-03-01 19:58:13 +0100

    build: add `digest` and `serialize` exports for forward compat (v1)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-export { objectHash } from "./object-hash";
+export { objectHash, objectHash as serialize /* v2 */ } from "./object-hash";
 export { hash } from "./hash";
 export { murmurHash } from "./crypto/murmur";
-export { sha256, sha256base64 } from "./crypto/sha256";
+export {
+  sha256,
+  sha256base64,
+  sha256base64 as digest /* v2 */,
+} from "./crypto/sha256";
 export { isEqual } from "./utils";
 export { diff } from "./diff";
