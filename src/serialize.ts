342b8212f497047668c6650b69a388e07b205231
Balázs Németh <zsilbi@zsilbi.hu>
2025-03-04 00:11:14 +0100

    fix(serialize): allow `input.toJSON()` to return any type

--- src/serialize.ts
+++ src/serialize.ts
@@ -97,7 +97,11 @@ const Serializer = /*@__PURE__*/ (function () {
         return this.serializeBuiltInType(objName, object);
       }
       if (typeof object.toJSON === "function") {
-        return objName + this.$object(object.toJSON());
+        const json = object.toJSON();
+        if (typeof json === "object" && json !== null) {
+          return objName + this.$object(json);
+        }
+        return objName + `(${this.serialize(json)})`;
       }
       return this.serializeObjectEntries(objName, Object.entries(object));
     }
