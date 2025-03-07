0da1631004d48e7b8b96f925b8c69826aa66c9f2
Balázs Németh <zsilbi@zsilbi.hu>
2025-03-07 11:57:12 +0100

    test(utils): add additional test cases for `isEqual()`

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,14 +1,31 @@
 import { describe, expect, it } from "vitest";
-import { isEqual, diff } from "../src/utils";
+import { diff, isEqual } from "../src/utils";
 
 describe("isEqual", () => {
+  class Test {}
+
   const cases = [
     [{ foo: "bar" }, { foo: "bar" }, true],
     [{ foo: "bar" }, { foo: "baz" }, false],
+    [{ foo: "bar" }, "baz", false],
+    [{ foo: "bar" }, 1, false],
     [{ a: 1, b: 2 }, { b: 2, a: 1 }, true],
+    [123, "foo", false],
     [123, 123, true],
     [123, 456, false],
     [[1, 2], [2, 1], false],
+    ["foo", [1, 2], false],
+    ["foo", "bar", false],
+    ["foo", "foo", true],
+    [0, 1, false],
+    [1, 1, true],
+    [new Test(), new Test(), true],
+    [new (class Test2 {})(), new (class Test2 {})(), true],
+    [new Test(), new (class Test2 {})(), false],
+    [Number.NaN, Number.NaN, true],
+    [Number.NaN, 0, false],
+    [undefined, 0, false],
+    [undefined, null, false],
   ];
   for (const [obj1, obj2, equals] of cases) {
     it(`${JSON.stringify(obj1)} ${
